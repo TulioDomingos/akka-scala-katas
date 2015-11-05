3ae8655b7edc605cd4f1c182c29ed176d76a955d
Tulio <tulio.domingos@king.com>
2015-11-05 15:11:54 +0100

    Removed implementation of Slave actor

--- master-slave/src/main/scala/katas/akka/masterslave/SlaveActor.scala
+++ master-slave/src/main/scala/katas/akka/masterslave/SlaveActor.scala
@@ -6,7 +6,5 @@ import akka.actor.Actor
  * Created by Tulio Domingos
  */
 class SlaveActor extends Actor {
-  def receive = {
-    case ReverseWord(word: String) => sender ! ReversedWord(word.reverse)
-  }
+  def receive = ???
 }
